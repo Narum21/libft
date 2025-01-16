83e671c0d4d691a697ded656737fff75e3e8fa0b
Narum21 <felixgodino.fg@gmail.com>
2025-01-16 16:47:22 +0100

    l

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,23 +12,24 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t lon)
 {
-    size_t j;
-    size_t  i;
-    size_t lon;
-    if  (!little && !needle)
-        return (NULL);
-    if (ft_strlen(needle) == 0)
-	    return ((char *) big);
-    lon = ft_strlen(needle);
-    i = 0;
-    while (big[i] && i < lon)
-    {
-        j = 0;
-        while (big[i + j] && big[i + j] == needle[j] && i + j < lon)
-            j++;
-        if (j == lon)
-                return ((char *) big + i);
-        i++;
-    }
-    return (NULL);
+	size_t	j;
+	size_t	i;
+	size_t	lon;
+
+	if (!little && !needle)
+		return (NULL);
+	if (ft_strlen(needle) == 0)
+		return ((char *) big);
+	lon = ft_strlen(needle);
+	i = 0;
+	while (big[i] && i < lon)
+	{
+		j = 0;
+		while (big[i + j] && big[i + j] == needle[j] && i + j < lon)
+			j++;
+		if (j == lon)
+			return ((char *) big + i);
+		i++;
+	}
+	return (NULL);
 }
